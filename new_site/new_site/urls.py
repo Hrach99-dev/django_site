40cc10e6370440c2ce26e1ef6d581fc3752b2bf3
Hrach Ghazaryan <hrach.ghazaryan.web@gmail.com>
2021-07-06 00:21:28 +0400

    Vse bug@  fixel em

--- new_site/new_site/urls.py
+++ new_site/new_site/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('new_site.urls')),
+    path('', include('newapp.urls')),
 ]
---
+++ new_site/newapp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+
+
+# Create your views here.
+
+
+# class-nerov mi ereq viewser@ ------------------<<<<<<
+
+def index(request):
+    return render(request, 'index.html')
