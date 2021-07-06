8800e30c0aefb5d0273c10a33e4831931a4963ac
Hayk1999 <hayknikogosyan60@gmail.com>
2021-07-06 15:16:16 +0400

    my coomt

--- new_site/newapp/admin.py
+++ new_site/newapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import CustomUser, Product, Buyer
 # Register your models here.
+
+admin.site.register(CustomUser)
+admin.site.register(Product)
+admin.site.register(Buyer)
