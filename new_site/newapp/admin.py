a59d7908f2531bcf3a77309eafa731c2b4e14549
Hrach Ghazaryan <hrach.ghazaryan.web@gmail.com>
2021-07-09 00:38:34 +0400

    Modelneri mas@ erel em

--- new_site/newapp/admin.py
+++ new_site/newapp/admin.py
@@ -1,5 +1,30 @@
 from django.contrib import admin
 
 # Register your models here.
-# from .models import *
+from .models import *
 
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'name', 'surname', 'password',
+    'admin')
+    list_filter = ('email', 'admin')
+    search_fields = ('name', 'surname')
+
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'price', 'timestamp',
+    'published')
+    list_filter = ['title', 'price', 'published']
+    search_fields = ['title']
+    ordering = ['price']
+
+
+@admin.register(Buyer)
+class BuyerAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'product_id')
+    list_filter = ['product_id']
+    search_fields = ['user_id']
